6cefadc40ac8b3afefbf7b389cf1b029ee0a9cd5
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2015-10-19 17:04:47 +1100

    Initial Commit

---
+++ TestableAlertViewController/TestableAlertViewController.h
@@ -0,0 +1,19 @@
+//
+//  TestableAlertViewController.h
+//  TestableAlertViewController
+//
+//  Created by Evgenii Neumerzhitckii on 19/10/2015.
+//  Copyright © 2015 Marketplacer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestableAlertViewController.
+FOUNDATION_EXPORT double TestableAlertViewControllerVersionNumber;
+
+//! Project version string for TestableAlertViewController.
+FOUNDATION_EXPORT const unsigned char TestableAlertViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestableAlertViewController/PublicHeader.h>
+
+
